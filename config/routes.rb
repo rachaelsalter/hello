7a83d8237d26b467885685341c377882e667bdcf
Rachael Salter <rasalter@gmail.com>
2015-07-31 00:42:25 +0000

    Adding background image and headine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Hello::Application.routes.draw do
+  root 'greetings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
